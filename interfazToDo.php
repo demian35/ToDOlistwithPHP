96f48f967e9a803ed057d94daba716e5103f2e93
Demian <53890358+demian35@users.noreply.github.com>
2023-07-24 02:43:37 -0600

    Esqueleto interfaz del todolist

---
+++ interfazToDo.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To Do list</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+</head>
+<body>
+    <form class="formularioTareas" action="" method="post">
+        Ingrese una tarea <input class="campotarea" required type="text" name="tarea">
+        <br>
+        <button class="btn btn-success" type="submit">Agrega tarea</button>
+    </form>
+
+    <table class="tabla">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Tarea</th>
+                            <th>Accion</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                        <tr class="">
+                            <td>  </td>
+                            <td></td>
+                            <td><a name="" id="" class="btn btn-danger" href="" role="button">Borrar</a></td>
+                        </tr>
+                       
+                    </tbody>
+                </table>
+
+
+</body>
+</html>
