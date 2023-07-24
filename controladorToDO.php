4c0c8be3ba4c62d3edc480a28ca156079a81e345
Demian <53890358+demian35@users.noreply.github.com>
2023-07-24 02:52:21 -0600

    funcionalidad para mostrar los resultados del select

---
+++ controladorToDO.php
@@ -0,0 +1,26 @@
+<?php
+
+$server="localhost";//nombre del server
+$usuario="root";//Usuario
+$password="root";//contraseña
+$baseDeDatos="basededatospracticapdo";//nombre de la base de datos en donde trabajaremos
+
+//creamos la conexion 
+$coneccion= new PDO("mysql:host=$server;dbname=$basedatos",$user,$password);
+$coneccion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);//manejo de errores
+
+//recepcion de datos
+if($_POST){
+
+}
+
+if($_GET){
+    
+}
+
+$sentencia=$coneccion->prepare("SELECT * FROM tareas");//consultamos lo que hay en la base de datos
+$sentencia->execute();//ejecutamos la sentenica
+
+$resultados=$sentencia->fetchAll();//regresamos los registros de la base de datos
+
+?>
